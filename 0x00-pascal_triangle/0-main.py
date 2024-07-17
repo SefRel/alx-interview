b5ac535f8f1f0030755d0de4673efd5e4047d27c
Ridwan Akinola <akinolaridwanbnhusaain@gmail.com>
2024-07-16 17:34:53 -0700

    module check update

--- 0x00-pascal_triangle/0-main.py
+++ 0x00-pascal_triangle/0-main.py
@@ -2,8 +2,31 @@
 """
 0-main
 """
-pascal_triangle = __import__('0-pascal_triangle').pascal_triangle
 
+def pascal_triangle(n):
+    if n <= 0:
+        return []
+    
+    # Initialize the triangle with the first row
+    triangle = [[1]]
+    
+    for i in range(1, n):
+        # Get the previous row
+        prev_row = triangle[-1]
+        # Initialize the current row
+        current_row = [1]
+        
+        # Calculate the values for the current row
+        for j in range(1, i):
+            current_row.append(prev_row[j - 1] + prev_row[j])
+        
+        # Add the last 1 in the current row
+        current_row.append(1)
+        
+        # Add the current row to the triangle
+        triangle.append(current_row)
+    
+    return triangle
 
 def print_triangle(triangle):
     """
@@ -12,6 +35,6 @@ def print_triangle(triangle):
     for row in triangle:
         print("[{}]".format(",".join([str(x) for x in row])))
 
-
 if __name__ == "__main__":
     print_triangle(pascal_triangle(5))
+
