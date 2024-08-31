cfc3c6a24e557a203aa2d93a437a885003565a3e
SefRel <ridwanakinolaclass@gmail.com>
2024-08-31 18:00:01 +0100

    Hot fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,51 +1,60 @@
 #!/usr/bin/python3
-
-"""
-    Interview preparation exercise: Minimum Operations
-    Problem description:
-    In a text file, there is a single character H. Your text editor can
-    execute only two operations in this file: Copy All and Paste. Given
-    a number n, write a method that calculates the fewest number of operations
-    needed to result in exactly n H characters in the file.
-    Prototype: def minOperations(n)
-    Returns an integer
-    If n is impossible to achieve, return 0
-    Example:
-    n = 9
-    H => Copy All => Paste => HH => Paste =>HHH => Copy All
-    => Paste => HHHHHH => Paste => HHHHHHHHH
-    Number of operations: 6
-"""
-
-from math import sqrt
+'''Minimum Operations python3 challenge'''
 
 
 def minOperations(n):
-    """
-        Function that computes the minimum operations based on
-        the requirements specified above
-    """
-    if n <= 1:
+    '''calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    Returns:
+        Integer : if n is impossible to achieve, return 0
+    '''
+    pasted_chars = 1  # how many chars in the file
+    clipboard = 0  # how many H's copied
+    counter = 0  # operations counter
+
+    while pasted_chars < n:
+        # if did not copy anything yet
+        if clipboard == 0:
+            # copyall
+            clipboard = pasted_chars
+            # increment operations counter
+            counter += 1
+
+        # if haven't pasted anything yet
+        if pasted_chars == 1:
+            # paste
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
+            # continue to next loop
+            continue
+
+        remaining = n - pasted_chars  # remaining chars to Paste
+        # check if impossible by checking if clipboard
+        # has more than needed to reach the number desired
+        # which also means num of chars in file is equal
+        # or more than in the clipboard.
+        # in both situations it's impossible to achieve n of chars
+        if remaining < clipboard:
+            return 0
+
+        # if can't be devided
+        if remaining % pasted_chars != 0:
+            # paste current clipboard
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 1
+        else:
+            # copyall
+            clipboard = pasted_chars
+            # paste
+            pasted_chars += clipboard
+            # increment operations counter
+            counter += 2
+
+    # if got the desired result
+    if pasted_chars == n:
+        return counter
+    else:
         return 0
-
-    value = findPrimeFactor(n)
-    temp = n
-    result = 0
-    while value != temp:
-        result += value
-        temp /= value
-        value = findPrimeFactor(temp)
-
-    result += value
-    return int(result)
-
-
-def findPrimeFactor(n):
-    """
-        Function that finds the lowest prime factor of a number,
-        if the number is prime, returns the number itself
-    """
-    for i in range(2, int(sqrt(n)) + 1):
-        if n % i == 0:
-            return i
-    return n
